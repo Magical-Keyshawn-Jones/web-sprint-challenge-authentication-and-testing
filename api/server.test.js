d8e021a5b708f41c7c76b0d3676aee73fd109824
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-06-01 15:35:39 -0500

    finished adding test

--- api/server.test.js
+++ api/server.test.js
@@ -35,12 +35,14 @@ beforeEach( async () => {
 
 describe('Register Api', () => {
   let response;
+
   test('Register has correct body',  async () => {
     response = await request(server).post('/api/auth/register').send({
       username: 'something'
     })
     expect(response.body).toHaveProperty('message', 'password is required')
   })
+
   test('Register has correct success response' , async () => {
     response = await request(server).post('/api/auth/register').send({
       username: 'Kriegster',
@@ -49,5 +51,59 @@ describe('Register Api', () => {
     expect(response.body.id).toBeDefined()
     expect(response.body).toHaveProperty('username', 'Kriegster')
     expect(response.body.password).toBeDefined()
+  })
+})
+
+describe('Login Api', () => {
+  let response 
+
+  test('Login has correct success response', async () => {
+    response = await request(server).post('/api/auth/register').send({
+      username: 'Kriegster',
+      password: 'DahBaby'
+    })
+    response = await request(server).post('/api/auth/login').send({
+      username: 'Kriegster',
+      password: 'DahBaby'
+    })
+    expect(response.body).toHaveProperty('message', 'Welcome Kriegster')
+    expect(response.body.token).toBeDefined()
+  })
+
+  test('Login fails if username does not exist in the database', async () => {
+    response = await request(server).post('/api/auth/login').send({
+      username: 'Kriegster',
+      password: 'DahBaby'
+    })
+    expect(response.status).toBe(401)
+  })
+})
+
+describe('Jokes Api', () => {
+  let response 
+
+  test('Jokes has correct success response', async ()  => {
+    response = await request(server).post('/api/auth/register').send({
+      username: 'Kriegster',
+      password: 'DahBaby'
+    })
+
+    response = await request(server).post('/api/auth/login').send({
+      username: 'Kriegster',
+      password: 'DahBaby'
+    })
+
+    const token = response.body.token
+    
+    response = await request(server).get('/api/jokes').send({
+      headers: {
+        authorization: token
+      }
+    })
+    expect(response.body).toBeDefined()
+  })
+  test('Jokes fails without a token', async ()  => {
+    response = await request(server).get('/api/jokes')
+    expect(response.status).toBe(400)
   })
 })
